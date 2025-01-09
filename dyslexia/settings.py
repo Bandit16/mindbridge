2e5ccd54d6dd3c983563f9d6c97ada34ff2b2181
Dipesh Acharya <dipeshacharya87@gmail.com>
2025-01-09 14:25:52 +0545

    Update settings.py

--- dyslexia/settings.py
+++ dyslexia/settings.py
@@ -137,7 +137,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SITE_ID = 1
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_TRUSTED_ORIGINS = [
     'https://pleasing-guppy-hardy.ngrok-free.app',
 ]
