4f2494b805637a9f337d6f081fa4d1448b68fd63
Dipesh Acharya <dipeshacharya87@gmail.com>
2025-05-03 21:34:59 +0545

    updated for deployment

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ from api.views import *
 urlpatterns = [
     path('api/alphabet-images/<str:letter>/', AlphabetImagesAPI.as_view(), name='alphabet-images'),
     path('api/game-images/<str:letter>/',GameImagesAPI.as_view(),name='game-images') ,
-    path('test-firebase/', test_firebase, name='test-firebase'),
-    path('sync-users/', SyncFirebaseUsers.as_view(), name='sync-users'),
-    path('api/recognize-voice/', VoiceRecognitionAPIView.as_view(), name='recognize-voice'),
-    path('sync-progress/', SyncProgressData.as_view(), name='save-progress'),
-    path('api/progress/<str:uuid>', ProgressDataApi.as_view(), name='progress'),
+    # path('test-firebase/', test_firebase, name='test-firebase'),
+    # path('sync-users/', SyncFirebaseUsers.as_view(), name='sync-users'),
+    # path('api/recognize-voice/', VoiceRecognitionAPIView.as_view(), name='recognize-voice'),
+    # path('sync-progress/', SyncProgressData.as_view(), name='save-progress'),
+    # path('api/progress/<str:uuid>', ProgressDataApi.as_view(), name='progress'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from .models import LetterImage, Letter , GameImage 
 from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from django.http import JsonResponse
-from firebase_admin import auth
-from firebase_admin import firestore
+# from firebase_admin import auth
+# from firebase_admin import firestore
 from .models import User , TestProgress
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -13,7 +13,7 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
-import speech_recognition as sr
+# import speech_recognition as sr
 import os
 
 
@@ -80,152 +80,152 @@ class GameImagesAPI(GenericAPIView):
 
 
 
-def test_firebase(request):
-    try:
-        # Retrieve a list of users (for testing purposes)
-        users = auth.list_users().iterate_all()
-        user_list = [{"uid": user.uid, "email": user.email} for user in users]
-        return JsonResponse({"users": user_list})
-    except Exception as e:
-        return JsonResponse({"error": str(e)}, status=500)
+# def test_firebase(request):
+#     try:
+#         # Retrieve a list of users (for testing purposes)
+#         users = auth.list_users().iterate_all()
+#         user_list = [{"uid": user.uid, "email": user.email} for user in users]
+#         return JsonResponse({"users": user_list})
+#     except Exception as e:
+#         return JsonResponse({"error": str(e)}, status=500)
     
 
     
 
 
-class SyncFirebaseUsers(APIView):
-    queryset = User.objects.all()  # Required for permissions like DjangoModelPermissions
-    permission_classes = [AllowAny]
+# class SyncFirebaseUsers(APIView):
+#     queryset = User.objects.all()  # Required for permissions like DjangoModelPermissions
+#     permission_classes = [AllowAny]
 
-    def post(self, request):
-        try:
-            users = auth.list_users().iterate_all()
-            created_count = 0
-            for user in users:
-                obj, created = User.objects.get_or_create(
-                    firebase_uid=user.uid,
-                    defaults={
-                        'email': user.email,
-                        'name': user.display_name or "Anonymous"
-                    }
-                )
-                if created:
-                    created_count += 1
-            return Response({
-                "message": f"Firebase users synced successfully. {created_count} users created."
-            })
-        except Exception as e:
-            return Response({"error": str(e)}, status=500)
+#     def post(self, request):
+#         try:
+#             users = auth.list_users().iterate_all()
+#             created_count = 0
+#             for user in users:
+#                 obj, created = User.objects.get_or_create(
+#                     firebase_uid=user.uid,
+#                     defaults={
+#                         'email': user.email,
+#                         'name': user.display_name or "Anonymous"
+#                     }
+#                 )
+#                 if created:
+#                     created_count += 1
+#             return Response({
+#                 "message": f"Firebase users synced successfully. {created_count} users created."
+#             })
+#         except Exception as e:
+#             return Response({"error": str(e)}, status=500)
         
-class SaveProgress(APIView):
-    permission_classes = [AllowAny]
+# class SaveProgress(APIView):
+#     permission_classes = [AllowAny]
 
-    def post(self, request):
-        try:
-            firebase_uid = request.data.get('firebase_uid')
-            test_id = request.data.get('test_id')
-            score = request.data.get('score')
-            time_spent = request.data.get('time_spent')  # e.g., '00:10:30'
+#     def post(self, request):
+#         try:
+#             firebase_uid = request.data.get('firebase_uid')
+#             test_id = request.data.get('test_id')
+#             score = request.data.get('score')
+#             time_spent = request.data.get('time_spent')  # e.g., '00:10:30'
 
-            user = User.objects.get(firebase_uid=firebase_uid)
-            TestProgress.objects.create(
-                user=user,
-                test_id=test_id,
-                score=score,
-                time_spent=time_spent,
-            )
-            return Response({"message": "Progress saved successfully."})
-        except User.DoesNotExist:
-            return Response({"error": "User not found."}, status=404)
-        except Exception as e:
-            return Response({"error": str(e)}, status=500)
+#             user = User.objects.get(firebase_uid=firebase_uid)
+#             TestProgress.objects.create(
+#                 user=user,
+#                 test_id=test_id,
+#                 score=score,
+#                 time_spent=time_spent,
+#             )
+#             return Response({"message": "Progress saved successfully."})
+#         except User.DoesNotExist:
+#             return Response({"error": "User not found."}, status=404)
+#         except Exception as e:
+#             return Response({"error": str(e)}, status=500)
 
 
-class VoiceRecognitionAPIView(APIView):
-    permission_classes = [AllowAny]
-    parser_classes = (MultiPartParser, FormParser)
+# class VoiceRecognitionAPIView(APIView):
+#     permission_classes = [AllowAny]
+#     parser_classes = (MultiPartParser, FormParser)
 
-    def post(self, request):
-        audio_file = request.FILES.get('audio')
-        if not audio_file:
-            return Response({"error": "No audio file provided"}, status=400)
+#     def post(self, request):
+#         audio_file = request.FILES.get('audio')
+#         if not audio_file:
+#             return Response({"error": "No audio file provided"}, status=400)
 
-        # Save the audio file temporarily
-        temp_file_path = "temp_audio.wav"
-        with open(temp_file_path, "wb") as f:
-            for chunk in audio_file.chunks():
-                f.write(chunk)
+#         # Save the audio file temporarily
+#         temp_file_path = "temp_audio.wav"
+#         with open(temp_file_path, "wb") as f:
+#             for chunk in audio_file.chunks():
+#                 f.write(chunk)
 
-        # Recognize voice
-        recognizer = sr.Recognizer()
-        try:
-            with sr.AudioFile(temp_file_path) as source:
-                audio = recognizer.record(source)
-                text = recognizer.recognize_google(audio, language='en-US', show_all=False)
-                os.remove(temp_file_path)  # Clean up temp file
-                return Response({"text": text}, status=200)
-        except sr.UnknownValueError:
-            return Response({"error": "Could not understand audio"}, status=400)
-        except sr.RequestError:
-            return Response({"error": "Speech Recognition service error"}, status=500)
-        except Exception as e:
-            return Response({"error": str(e)}, status=500)
+#         # Recognize voice
+#         recognizer = sr.Recognizer()
+#         try:
+#             with sr.AudioFile(temp_file_path) as source:
+#                 audio = recognizer.record(source)
+#                 text = recognizer.recognize_google(audio, language='en-US', show_all=False)
+#                 os.remove(temp_file_path)  # Clean up temp file
+#                 return Response({"text": text}, status=200)
+#         except sr.UnknownValueError:
+#             return Response({"error": "Could not understand audio"}, status=400)
+#         except sr.RequestError:
+#             return Response({"error": "Speech Recognition service error"}, status=500)
+#         except Exception as e:
+#             return Response({"error": str(e)}, status=500)
         
 
 
-class SyncProgressData(APIView):
-    permission_classes = [AllowAny]
+# class SyncProgressData(APIView):
+#     permission_classes = [AllowAny]
 
-    def post(self, request):
-        try:
-            db = firestore.client()
-            progress_docs = db.collection('progress').stream()
+#     def post(self, request):
+#         try:
+#             db = firestore.client()
+#             progress_docs = db.collection('progress').stream()
 
-            created_count = 0
-            for doc in progress_docs:
-                data = doc.to_dict()
-                firebase_uid = data.get('firebase_uid')
-                test_id = data.get('testid')
-                score = data.get('score')
-                time_spent = data.get('timespent')  
+#             created_count = 0
+#             for doc in progress_docs:
+#                 data = doc.to_dict()
+#                 firebase_uid = data.get('firebase_uid')
+#                 test_id = data.get('testid')
+#                 score = data.get('score')
+#                 time_spent = data.get('timespent')  
 
-                # Fetch corresponding user
-                try:
-                    user = User.objects.get(firebase_uid=firebase_uid)
-                except User.DoesNotExist:
-                    print("user doesnt exists")
-                    continue 
+#                 # Fetch corresponding user
+#                 try:
+#                     user = User.objects.get(firebase_uid=firebase_uid)
+#                 except User.DoesNotExist:
+#                     print("user doesnt exists")
+#                     continue 
 
-                # Create ProgressReport entry
-                TestProgress.objects.create(
-                    user=user,
-                    test_id=test_id,
-                    score=score,
-                    time_spent=time_spent,
-                )
-                created_count += 1
+#                 # Create ProgressReport entry
+#                 TestProgress.objects.create(
+#                     user=user,
+#                     test_id=test_id,
+#                     score=score,
+#                     time_spent=time_spent,
+#                 )
+#                 created_count += 1
 
-            return Response({
-                "message": f"Progress data synced successfully. {created_count} records created."
-            })
-        except Exception as e:
-            return Response({"error": str(e)}, status=500)
+#             return Response({
+#                 "message": f"Progress data synced successfully. {created_count} records created."
+#             })
+#         except Exception as e:
+#             return Response({"error": str(e)}, status=500)
         
-class ProgressDataApi(APIView):
-    permission_classes = [AllowAny]
-    queryset = TestProgress.objects.all()
+# # class ProgressDataApi(APIView):
+# #     permission_classes = [AllowAny]
+# #     queryset = TestProgress.objects.all()
 
-    def get(self, request , uuid):
-        try:
+# #     def get(self, request , uuid):
+# #         try:
             
-            progress_data = TestProgress.objects.get(user__firebase_uid=uuid)
-            response_data = {
-                "user": progress_data.user.name,
-                "test_id": progress_data.test_id,
-                "score": progress_data.score,
-                "time_spent": progress_data.time_spent,
-                "created_at": progress_data.created_at,
-            }
-            return JsonResponse({"data": response_data})
-        except Exception as e:
-            return JsonResponse({"error": str(e)}, status=500)
+# #             progress_data = TestProgress.objects.get(user__firebase_uid=uuid)
+# #             response_data = {
+# #                 "user": progress_data.user.name,
+# #                 "test_id": progress_data.test_id,
+# #                 "score": progress_data.score,
+# #                 "time_spent": progress_data.time_spent,
+# #                 "created_at": progress_data.created_at,
+# #             }
+# #             return JsonResponse({"data": response_data})
+# #         except Exception as e:
+# #             return JsonResponse({"error": str(e)}, status=500)
